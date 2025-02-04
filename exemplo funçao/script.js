2d67b0b83ef044964dc4066525d1a170ac78439e
Laís Boscolo <costaboscololais2008@gmail.com>
2025-02-04 10:21:45 -0300

    functions exemplo

---
+++ exemplo funçao/script.js
@@ -0,0 +1,13 @@
+//EXEMPLO 1
+//function teste (){
+//alert ("socorro")
+//}
+
+//teste ()
+
+const clicou =()=> {
+    window.alert("voce clicou no botao 😍😘🤩😶😮")
+}
+
+//const= variavel que se repete
+//window alert = janela de alerta ai aparece a mensagem nos parenteses
