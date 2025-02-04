f137e47f40024a89c579da4448f6f96f3b21f318
Laís Boscolo <costaboscololais2008@gmail.com>
2025-02-04 10:22:31 -0300

    exemplo function soma

---
+++ exemplo funçao/somar funçao/script.js
@@ -0,0 +1,9 @@
+
+const soma = ()=>{
+
+    let nu1 =  parseFloat(document.getElementById("n1").value)
+    let nu2 =  parseFloat(document.getElementById("n2").value)
+    let resultado = (nu1) + (nu2)
+    window.alert("Olá, a soma dos números é " + resultado)
+
+}
