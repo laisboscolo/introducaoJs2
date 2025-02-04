e2147553d6233453ce5e6d2c2b0d4044570cac74
Laís Boscolo <costaboscololais2008@gmail.com>
2025-02-04 10:22:59 -0300

    exemplo prompt

---
+++ exemplo prompt/script.js
@@ -0,0 +1,7 @@
+let nome = prompt("digite o seu nome")
+
+let idade = prompt("Digite sua idade")
+
+document.getElementById("nomeidade").innerText = "Olá : " + nome + ", você tem : " + idade + " anos."
+ 
+
