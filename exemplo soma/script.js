d640b18f8fa6ddb34cd3f3c87de9cf51ed403181
Laís Boscolo <costaboscololais2008@gmail.com>
2025-02-04 10:23:19 -0300

    exemplo soma

---
+++ exemplo soma/script.js
@@ -0,0 +1,24 @@
+//let n1 = (5) //pra somar colocar o numero no parenteses e depois colocar let soma(funçao) para somar dps colocar o document pra ter o resultado na pagina
+//let n2 = (9)
+//let soma = n1 + n2
+
+//document.getElementById("resultados").innerText =  n1 + n2 
+
+// antes no prompt nao dava pq o sistema enxergava como texto entao so juntava os numeros 
+
+//let n1 = prompt ("digite o primeiro numero")
+//let n2 = prompt ("digite o segundo numero")
+
+//document.getElementById("resultados").innerText =  n1 + n2 
+
+//let n1 = prompt("digite o primeiro numero")//parseint ele soma os numeros mas tem que colocar antes do promp para identificar que um numero
+//let n2 = prompt("digite o segundo numero")
+//let resultado = (parseInt(n1) + perseInt(n2))
+
+//document.getElementById("resultados").innerText ="seu resultado é" + resultado
+
+let n1 = prompt("Digite o primeiro número")
+let n2 = prompt("Digite o segundo número")
+let resultado = (parseInt(n1)+ parseInt(n2))
+
+document.getElementById("resultados").innerText = "Olá, seu resultado é " + resultado
