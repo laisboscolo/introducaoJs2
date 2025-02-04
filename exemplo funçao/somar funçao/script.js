49e251add613ab6494dad4c2b751db162e155362
Laís Boscolo <costaboscololais2008@gmail.com>
2025-02-04 13:40:12 -0300

    somar

--- exemplo funçao/somar funçao/script.js
+++ exemplo funçao/somar funçao/script.js
@@ -1,8 +1,8 @@
 
 const soma = ()=>{
-
-    let nu1 =  parseFloat(document.getElementById("n1").value)
-    let nu2 =  parseFloat(document.getElementById("n2").value)
+//getelement r para puxar o ID
+    let nu1 =  parseFloat(document.getElementById("n1").value)// document para ir ao html |parsefloat pra somar numero decimal
+    let nu2 =  parseFloat(document.getElementById("n2").value)//value= pra dizer que o valor tem que ir para a variavel
     let resultado = (nu1) + (nu2)
     window.alert("Olá, a soma dos números é " + resultado)
 
