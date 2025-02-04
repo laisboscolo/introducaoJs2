f830dd088b12889a0948c89c85a6cb455a205b35
Laís Boscolo <costaboscololais2008@gmail.com>
2025-02-04 10:23:30 -0300

    exemplo variavel

---
+++ exemplo variavel/script.js
@@ -0,0 +1,24 @@
+//tipo de variavel
+// let = tipo de variavel que serve so para um bloco especifico
+//let nome = ("Laís") // spring
+//console.log(nome) // isso é uma declaração
+//alert(nome)//aparece o let nome 
+//let bairro = ("Parque erasmo assunçao")
+//console.log(bairro) // isso é uma declaração
+
+
+//document.getElementById("resultado").innerText = "Olá,seu nome é: " + nome//procura documento que tem id ou seja o resultado |innerText=tipo de linkamento com o p |string
+//contatenar = unir os dados
+
+
+//document.getElementeById("resultado").innerText = "Olá :," + nome + " , voce mora no bairro : " + bairro + ", boa tarde"
+
+
+let nome = ("Lais")
+console.log(nome)
+alert(nome)
+let bairro = ("São Francisco")
+console.log(bairro)
+
+document.getElementById("resultado").innerText = "Olá : " + nome + ", você mora no bairro : " + bairro + ", boa tarde."
+ 
